33ef110a99d24f4822382d512497a3b66b2c0dac
Michael L. Collard <collard@uakron.edu>
2023-01-05 14:26:39 -0500

    Add initial files

---
+++ rainfall.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<float> rainfall;
+    float t;
+    float m;
+    float n;
+    while (cin >> n) {
+        rainfall.push_back(n);
+    }
+    if (!rainfall.size()) {
+        cout << "Error: no rainfall data";
+        return 1;
+    }
+    t = rainfall[0];
+    m = rainfall[0];
+    for (vector<float>::size_type i = 1; i < rainfall.size(); ++i) {
+        t += rainfall[i];
+        if (rainfall[i] > m)
+            m = rainfall[i];
+    }
+    // cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << (t / rainfall.size()) << " hundreds of inches" << '\n';
+    // cout << "Heaviest Hourly Rainfall: " << m << " hundreds of inches" << '\n';
+    cout << "| Hourly Rainfall | Inches in 100s |" << '\n';
+    cout << "|:----------------|----------------|" << '\n';
+    cout << "| Average         |     " << left << setw(10) << fixed << setprecision(2) << (t / rainfall.size()) << " |" << '\n';
+    cout << "| Heaviest        |     " << left << setw(10) << m << " |" << '\n';
+}
